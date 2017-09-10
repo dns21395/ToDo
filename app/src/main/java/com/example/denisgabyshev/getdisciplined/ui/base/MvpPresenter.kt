483f29afbf4e40900cdf03390d1b21f7fa528628
dns21395 <dns21395@gmail.com>
2017-09-10 16:44:06 +0900

    base interface presenter and view

---
+++ app/src/main/java/com/example/denisgabyshev/getdisciplined/ui/base/MvpPresenter.kt
@@ -0,0 +1,11 @@
+package com.example.denisgabyshev.getdisciplined.ui.base
+
+/**
+ * Created by denisgabyshev on 10/09/2017.
+ */
+interface MvpPresenter<V> : MvpView {
+    fun onAttach(V: MvpView)
+
+    fun onDetach()
+}
+
---
+++ app/src/main/java/com/example/denisgabyshev/getdisciplined/ui/base/MvpView.kt
@@ -0,0 +1,12 @@
+package com.example.denisgabyshev.getdisciplined.ui.base
+
+/**
+ * Created by denisgabyshev on 10/09/2017.
+ */
+interface MvpView {
+    fun hideKeyboard()
+
+    fun showToast()
+
+
+}
