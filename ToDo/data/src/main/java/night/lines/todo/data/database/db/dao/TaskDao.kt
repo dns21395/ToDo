f2d23fb312d74c98495db15c431a68cad7cd2981
Denis Gabyshev <dns21395@gmail.com>
2018-05-04 11:15:16 +0900

    testing

--- ToDo/data/src/main/java/night/lines/todo/data/database/db/dao/TaskDao.kt
+++ ToDo/data/src/main/java/night/lines/todo/data/database/db/dao/TaskDao.kt
@@ -3,6 +3,7 @@ package night.lines.todo.data.database.db.dao
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Query
 import io.reactivex.Flowable
+import io.reactivex.Observable
 import night.lines.todo.data.database.db.model.TaskModel
 
 /**
@@ -13,4 +14,7 @@ interface TaskDao : BaseDao<TaskModel> {
 
     @Query("SELECT * FROM task WHERE isDone = :finished OR isDone = 0")
     fun getTasks(finished: Int): Flowable<List<TaskModel>>
+
+    @Query("SELECT * FROM task WHERE date = :date LIMIT 1")
+    fun getTaskByDate(date: Long): TaskModel
 }
